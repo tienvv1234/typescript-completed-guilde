f772b4647a5f6b3eac741ede1d2fe582ae41ee73
tienvv1234 <tienvv1234@gmail.com>
2021-02-05 17:10:48 +0700

    update

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,21 @@
-console.log('test')
+import fs from 'fs';
+
+const matches = fs.readFileSync('./stats/football.csv', {
+    encoding: 'utf-8'
+}).split('\n').map((row: string): string[] => {
+    return row.split(',')
+});
+
+console.log(matches);
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+    if(match[1] === 'Man United' && match[5] === 'H'){
+        manUnitedWins++;
+    }else if (match[2] === 'Man united' && match[5] === 'A') {
+        manUnitedWins++;
+    }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
