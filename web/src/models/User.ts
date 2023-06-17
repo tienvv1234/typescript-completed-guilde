064a8ff1789a3ae54656f77c96b2e845959f624a
tienvv1234 <tienvv1234@gmail.com>
2023-06-17 09:24:03 +0700

    create event class

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -5,10 +5,7 @@ interface UserProps {
     age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-    events: {[key: string]: Callback[] } = {};
 
     constructor(private data: UserProps) {}
 
@@ -18,23 +15,6 @@ export class User {
 
     set(update: UserProps): void {
         Object.assign(this.data, update)
-    }
-
-    on(eventName: string, callback: Callback): void {
-        const handlers = this.events[eventName] || []
-        handlers.push(callback);
-        this.events[eventName] = handlers;
-    }
-
-    trigger(eventName: string): void {
-        const handlers = this.events[eventName];
-        if(!handlers || handlers.length === 0) {
-            return;
-        }
-
-        handlers.forEach(callback => {
-            callback();
-        })
     }
 
     async fetch(): Promise<AxiosResponse> {
